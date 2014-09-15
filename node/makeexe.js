ffabad8de43cbdeba379bfdf3b3406fbcb609526
shigemk2 <i.am.shige@gmail.com>
2014-09-15 17:51:09 +0900

    Add procedures

--- node/makeexe.js
+++ node/makeexe.js
@@ -64,3 +64,129 @@ function align(bytes, a) {
   return zero(a - m);
 }
 
+/**
+ * idata(idt ilt iat putchar dllname部分)のバイナリを出力する
+ *
+ * @method getidata
+ * @param なし
+ * @return {JSON} idata バイナリ文字列, putchar
+ */
+function getidata() {
+  var idata = "";
+
+  // idt
+  idata += convLEs(4, [0x2028, 0, 0, 0x2042, 0x2030]);
+  idata += zero(5 * 4);
+
+  // ilt
+  idata += convLEs(4, [0x2038, 0]);
+
+  // iat
+  var putchar = 0x402000 + idata.length;
+  idata += convLEs(4, [0x2038, 0]);
+
+  // putchar
+  idata += convLE(2, 0);
+  idata += "putchar\0";
+
+  // DLL name
+  idata += "msvcrt.dll\0";
+
+  return {"idata":idata, "putchar": putchar};
+}
+
+/**
+ * EXEの実際の処理部分
+ *
+ * @method gettext
+ * @param {String} idata idata部分のバイナリ文字列
+ * @return {String} text 処理のバイナリ文字列
+ */
+function gettext(idata, putchar) {
+  var text = "";
+  text += "\x6a\x41";         // push 0x41
+  text += "\xff\x15";         // call [putchar]
+  text += convLE(4, putchar);
+  text += "\x58";             // pop eax
+  text += "\xc3";             // ret
+  return text;
+}
+
+function main() {
+  var codes = "";
+
+  // DOS Header
+  codes += "MZ";
+  codes += convLEs(2, [0x90, 3, 0, 4, 0, 0xffff, 0, 0xb8, 0, 0, 0x40]);
+
+  // PE offset
+  codes += zero(0x3c - codes.length);
+  codes += convLE(4, 0x80);
+
+  // DOS stub
+  codes += "\x0e";         // push cs
+  codes += "\x1f";         // pop ds
+  codes += "\xba\x0e\x00"; // mov dx, 0xe
+  codes += "\xb4\x09";     // mov ah, 9
+  codes += "\xcd\x21";     // int 0x21
+  codes += "\xb8\x01\x4c"; // mov ax, 0x4c01
+  codes += "\xcd\x21";     // int 0x21
+  codes += "This program cannot be run in DOS mode.\r\r\n$";
+
+  codes += align(codes, 0x80);
+
+  // nth.FileHeader
+  var idataJSON = getidata();
+  var idata = idataJSON["idata"];
+  var putchar = idataJSON["putchar"];
+
+  codes += "PE\0\0";
+  codes += convLEs(2, [0x014c, 2]);
+  codes += convLEs(4, [0x4ada65f9b, 0, 0]);
+  codes += convLE (2, [0xe0, 0x0102]);
+
+  // nth.OptionalHeader
+  codes += convLE (2, 0x010b);
+  codes += convLEs(1, [10, 0]);
+  codes += convLEs(4, [0x0200, 0, 0, 0x1000, 0x1000, 0x2000,
+                      0x400000, 0x1000, 0x200]);
+  codes += convLEs(4, [5, 1, 0, 0, 5, 1]);
+  codes += convLEs(4, [0, 0x3000, 0x0200, 0]);
+  codes += convLEs(2, [3, 0]);
+  codes += convLEs(4, [0x100000, 0x1000, 0x100000, 0x1000, 0, 16]);
+  codes += convLEs(4, [0, 0, 0x2000, idata.length]);
+  codes += zero(14 * 8);
+
+  // sects .text
+  var text = gettext(idata, putchar);
+
+  codes += ".text";
+  codes += align(codes, 8);
+  codes += convLEs(4, [text.length, 0x1000, 0x0200, 0x0200, 0, 0]);
+  codes += convLEs(2, [0, 0]);
+  codes += convLE (4, 0x60000020);
+
+  // sects .idata
+  codes += ".idata";
+  codes += align(codes, 8);
+  codes += convLEs(4, [idata.length, 0x2000, 0x0200, 0x0400, 0, 0]);
+  codes += convLEs(2, [0, 0]);
+  codes += convLE (4, 0xc0300040);
+
+  codes += align(codes, 0x200);
+
+  // .text
+  codes += text;
+  codes += align(codes, 0x0200);
+
+  // .idata
+  codes += idata;
+  codes += align(codes, 0x0200);
+
+  return codes;
+}
+
+fs.writeFile("a.exe", main(), 'binary', function (err) {
+  // console.log(data);
+  // console.log(err);
+});
